7a1ffc65b18718dbda76048b6c65e7f63f568998
Faynot <faynot_youtube@mail.ru>
2024-05-19 19:01:16 +0300

    0.0.2.3

--- target/debug/build/serde_json-4f25dbf06ef4fca9/build_script_build-4f25dbf06ef4fca9.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Happy PC\RustroverProjects\protof\target\debug\build\serde_json-4f25dbf06ef4fca9\build_script_build-4f25dbf06ef4fca9.exe: C:\Users\Happy\ PC\.cargo\registry\src\index.crates.io-6f17d22bba15001f\serde_json-1.0.116\build.rs
-
-C:\Users\Happy PC\RustroverProjects\protof\target\debug\build\serde_json-4f25dbf06ef4fca9\build_script_build-4f25dbf06ef4fca9.d: C:\Users\Happy\ PC\.cargo\registry\src\index.crates.io-6f17d22bba15001f\serde_json-1.0.116\build.rs
-
-C:\Users\Happy\ PC\.cargo\registry\src\index.crates.io-6f17d22bba15001f\serde_json-1.0.116\build.rs:
--- target/debug/deps/same_file-0310b9b8f4f1e971.d
+++
@@ -1,8 +0,0 @@
-C:\Users\Happy PC\RustroverProjects\protof\target\debug\deps\libsame_file-0310b9b8f4f1e971.rmeta: C:\Users\Happy\ PC\.cargo\registry\src\index.crates.io-6f17d22bba15001f\same-file-1.0.6\src\lib.rs C:\Users\Happy\ PC\.cargo\registry\src\index.crates.io-6f17d22bba15001f\same-file-1.0.6\src\win.rs
-
-C:\Users\Happy PC\RustroverProjects\protof\target\debug\deps\libsame_file-0310b9b8f4f1e971.rlib: C:\Users\Happy\ PC\.cargo\registry\src\index.crates.io-6f17d22bba15001f\same-file-1.0.6\src\lib.rs C:\Users\Happy\ PC\.cargo\registry\src\index.crates.io-6f17d22bba15001f\same-file-1.0.6\src\win.rs
-
-C:\Users\Happy PC\RustroverProjects\protof\target\debug\deps\same_file-0310b9b8f4f1e971.d: C:\Users\Happy\ PC\.cargo\registry\src\index.crates.io-6f17d22bba15001f\same-file-1.0.6\src\lib.rs C:\Users\Happy\ PC\.cargo\registry\src\index.crates.io-6f17d22bba15001f\same-file-1.0.6\src\win.rs
-
-C:\Users\Happy\ PC\.cargo\registry\src\index.crates.io-6f17d22bba15001f\same-file-1.0.6\src\lib.rs:
-C:\Users\Happy\ PC\.cargo\registry\src\index.crates.io-6f17d22bba15001f\same-file-1.0.6\src\win.rs:
